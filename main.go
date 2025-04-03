3c0d919b489e910fc55746a47a278d91736dbd3e
DevKayoS <kayoribeirodev@gmail.com>
2025-04-02 23:43:41 -0300

    terminar amanah

--- main.go
+++ main.go
@@ -2,42 +2,42 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
-	"io"
+	"log"
+	"net"
 	"net/http"
 	"time"
 )
 
 func main() {
-	time.Sleep(time.Second)
-
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	req, err := http.NewRequestWithContext(
-		ctx,
-		http.MethodGet,
-		"http://localhost:8080",
-		nil,
+	mux := http.NewServeMux()
+	mux.HandleFunc(
+		"/healthcheck",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "hello, world")
+		},
 	)
 
-	if err != nil {
-		panic(err)
+	server := &http.Server{
+		Addr:                         ":8080",
+		Handler:                      mux,
+		DisableGeneralOptionsHandler: false,
+		TLSConfig:                    &tls.Config{},
+		ReadTimeout:                  10 * time.Second,
+		WriteTimeout:                 10 * time.Second,
+		IdleTimeout:                  1 * time.Minute,
+		MaxHeaderBytes:               0,
+		TLSNextProto:                 map[string]func(*http.Server, *tls.Conn, http.Handler){},
+		ConnState: func(net.Conn, http.ConnState) {
+			panic("TODO")
+		},
+		ErrorLog: &log.Logger{},
+		BaseContext: func(net.Listener) context.Context {
+			panic("TODO")
+		},
+		ConnContext: func(ctx context.Context, c net.Conn) context.Context {
+			panic("TODO")
+		},
 	}
-
-	req.Header.Set("accept", "application/json")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(data))
 }
